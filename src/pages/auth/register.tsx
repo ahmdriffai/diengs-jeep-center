afb2dd5267a57bf8c6e9d2332ec641002450b55d
Ahmad Rifai <ahmmd.riffai@gmail.com>
2025-06-26 15:34:34 +0700

    feat: drop register

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,12 +1,14 @@
-import RegisterView from "@/src/components/views/auth/Register";
-import React from "react";
+// import RegisterView from "@/src/components/views/auth/Register";
+// import Head from "next/head";
+// import React from "react";
 
-const RegisterPage: React.FC = () => {
-  return (
-    <>
-      <RegisterView />
-    </>
-  );
-};
+// const RegisterPage: React.FC = () => {
+//   return (
+//     <>
+//       <Head></Head>
+//       <RegisterView />
+//     </>
+//   );
+// };
 
-export default RegisterPage;
+// export default RegisterPage;
